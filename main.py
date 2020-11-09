32046afd6f63acbcccc54e7df1a9bf36c916319e
hjq133 <1399615054@qq.com>
2020-11-09 22:30:29 +0800

    rebot

--- main.py
+++ main.py
@@ -1,84 +1,79 @@
 import sys
 
-sign_map = {'+': 0,
-            '*': 1,
-            'i': 2,
-            '(': 3,
-            ')': 4
+priority = {'+': {'+': 1, '*': -1, 'i': -1, '(': -1, ')': 1, '#': 1},
+            '*': {'+': 1, '*': 1, 'i': -1, '(': -1, ')': 1, '#': 1},
+            'i': {'+': 1, '*': 1, 'i': None, '(': None, ')': 1, '#': 1},
+            '(': {'+': -1, '*': -1, 'i': -1, '(': -1, ')': 0, '#': None},
+            ')': {'+': 1, '*': 1, 'i': None, '(': None, ')': 1, '#': 1},
+            '#': {'+': -1, '*': -1, 'i': -1, '(': -1, ')': None, '#': -9}
             }
+grammar = ["E+E", "E*E", "(E)", "i"]
 
-matrix = [
-    [1, -1, -1, -1, 1],
-    [1, 1, -1, -1, 1],
-    [1, 1, None, None, 1],
-    [-1, -1, -1, -1, 0],
-    [1, 1, None, None, 1]
-]
-
-if sys.argv[0] == '/home/hjq133/myDesktop/编译作业/BUAA_Compile_2/main.py':
+if sys.argv[0] == '/home/hjq133/myDesktop/编译作业/BUAA_Compile_2/main2.py':
     file_path = '../data.txt'
 else:
     file_path = sys.argv[1]
-# file_path = '../data.txt'
 stack = []
-vn_stack = []
 
 f = open(file_path, 'r')
 l = f.readlines()[0].strip()
+l = l + "#"
 lst = list(l)
 
-i = 0
-while i < len(lst):
-    ch = lst[i]
-    if ch not in sign_map.keys():
-        print('E')
-        exit(0)
 
-    index = sign_map[ch]
-    if len(stack) == 0:  # 栈为空
-        stack.append(index)
-        print('I{}'.format(ch))
-        i += 1
-        continue
+def analyze():
+    stack.append("#")
 
-    priority = matrix[stack[-1]][index]  # 栈顶优先级
-    if priority == -1 or priority == 0:  # 栈内 >= 栈外, 移进
-        stack.append(index)
-        i += 1
-        print('I{}'.format(ch))
+    i = 0
+    while i < len(lst):
+        # 寻找栈中第一个终结符
+        ch = lst[i]
+        j = len(stack) - 1
+        while stack[j] == 'E':
+            j -= 1
 
-    elif priority == 1:  # 栈内 > 栈外
-        tmp = len(stack) - 1
-        if stack[tmp] == 2:
-            pass
-        elif (tmp == 0 or matrix[stack[tmp - 1]][stack[tmp]] == -1 or matrix[stack[tmp - 1]][stack[tmp]] == 0) and len(
-                vn_stack) != 0:  # 栈内 < 栈外
-            vn_stack.pop()
+        if priority[stack[j]][ch] == -1 or priority[stack[j]][ch] == 0:  # < =
+            move_in(ch)
+            i += 1
+        elif priority[stack[j]][ch] == 1:  # >
+            reduction(j)
+        elif priority[stack[j]][ch] == -9:
+            break
         else:
-            print("RE")
+            print("E")
             exit(0)
 
-        vn_stack.append(1)
-        stack = stack[:tmp]
-        print("R")
-        continue
-    else:
-        print('E')
+    if ''.join(stack) != "#E":
+        print("RE")
         exit(0)
 
-while len(stack) != 0:
-    tmp = len(stack) - 1
-    if stack[tmp] == 2:
-        pass
-    elif (tmp == 0 or matrix[stack[tmp - 1]][stack[tmp]] == -1 or matrix[stack[tmp - 1]][stack[tmp]] == 0) and len(
-            vn_stack) != 0:  # 栈内 < 栈外
-        vn_stack.pop()
+    return
+
+
+def move_in(ch):
+    print('I{}'.format(ch))
+    stack.append(ch)
+
+
+def reduction(j):  # i 栈外字符, j: 栈中第一个终结符
+    i = j
+    k = j-1
+    global stack
+    while stack[k] == 'E':  # 寻找栈中下一个终结符
+        k -= 1
+    while priority[stack[k]][stack[i]] != -1:  # 一直找到 < 符号
+        i = k
+        k -= 1
+        while stack[k] == 'E':  # 寻找栈中下一个终结符
+            k -= 1
+    if ''.join(stack[k+1:]) in grammar:
+        print("R")
+        stack = stack[:k+1]
+        stack.append("E")
     else:
         print("RE")
         exit(0)
-    vn_stack.append(1)
-    stack = stack[:tmp]
-    print("R")
-    continue
+    # k j 为待归约字符串
 
-exit(0)
+
+analyze()
